3221f97cbe0b30148af0541d984883349bc16652
Nick Woodhams <nicholas.woodhams@gmail.com>
2015-08-04 00:09:52 -0400

    added endpoint to add admin role to user

--- wsgi.py
+++ wsgi.py
@@ -17,9 +17,31 @@ from werkzeug.wsgi import DispatcherMiddleware
 
 from straply import api, frontend, restless
 
-application = DispatcherMiddleware(frontend.create_app(), {
-    '/api': api.create_app(),
-    '/restless': restless.create_app()
+frontend_app = frontend.create_app()
+api_app = api.create_app()
+restless_app = restless.create_app()
+
+
+@frontend_app.before_first_request
+def setup_app():
+    from straply.models import User, Role
+    from straply.core import db
+    if not Role.query.filter_by(name="admin").count():
+        role = Role(name="admin")
+        db.session.add(role)
+        db.session.commit()
+    else:
+        role = Role.query.filter_by(name='admin').first()
+    if User.query.filter_by().count() == 1:
+        user = User.query.filter_by().first()
+        user.roles = [role]
+        db.session.commit()
+    print("Setting up the app!")
+
+
+application = DispatcherMiddleware(frontend_app, {
+    '/api': api_app,
+    '/restless': restless_app
 })
 
 if __name__ == "__main__":
